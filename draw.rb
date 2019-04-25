91c86d016e736ff6534aee1adda52198d337a354
Callum Forbes <forbescallum@gmail.com>
2019-04-26 00:42:39 +0100

    Separate normalisation from color function

--- draw.rb
+++ draw.rb
@@ -3,7 +3,7 @@ include Magick
 require_relative 'mandelbrot'
 
 $WIDTH, $HEIGHT = 800, 800
-$MAX = 30.0
+$MAX = 50.0
 
 $image = Image.new($WIDTH, $HEIGHT) { self.background_color = "red" }
 
@@ -16,14 +16,14 @@ def plot
   verts.each { |x, y| $image.pixel_color(x, y, yield(x, y)) }
 end
 
-def mandelbrot_norm(x, y)
+def norm(x, y)
   x = (x - $WIDTH / 2.0) * 4.0 / $WIDTH
   y = (y - $HEIGHT / 2.0) * 4.0 / $HEIGHT
-  $mandelbrot.color_at(x, y, $MAX)
+  return x, y
 end
 
 def render
-  plot { |x,y| mandelbrot_norm(x, y) }
+  plot { |x,y| $mandelbrot.color_at(*norm(x, y), $MAX) }
   $image.display
 end
 
