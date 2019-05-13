879df333049ccb70f743d5604d2e705e7b02baf3
Callum Forbes <forbescallum@gmail.com>
2019-05-13 12:40:52 +0100

    use optparse for commandline interface

--- draw.rb
+++ draw.rb
@@ -1,6 +1,7 @@
 require 'rmagick'
 include Magick
 require_relative 'mandelbrot'
+require 'optparse'
 
 class Plot
   attr_reader :image
@@ -34,16 +35,38 @@ def plot_mandelbrot(width, height, iterations)
   return p
 end
 
-if ARGV.length >= 3
-  width, height = ARGV[0].to_i, ARGV[1].to_i, 
-  iterations = ARGV[2].to_f
-  p = plot_mandelbrot(width, height, iterations)
-  if ARGV[3]
-    p.image.write(ARGV[3])
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: draw.rb width height [options]"
+
+  opts.on("-i", "--iterations N", Integer,
+          "Number of iterations for each pixel. Default: 50") do |n|
+    options[:iterations] = n
+  end
+
+  opts.on("-f", "--filename FILENAME",
+          "Filename to save image to.\
+          If none provided will display in window.") do |filename|
+    options[:filename] = filename
+  end
+
+  opts.on("-h", "--help", "Prints this help") do
+    puts opts
+    exit
+  end
+end.parse!
+
+options[:iterations] ||= 50
+if ARGV[0] && ARGV[1]
+  width, height = ARGV.take(2).map(&:to_i)
+  p = plot_mandelbrot(width, height, options[:iterations])
+
+  if options[:filename]
+    p.image.write(options[:filename])
   else
     p.image.display
   end
 else
-  puts "Usage: draw.rb width height iterations [filename]"
-  puts "If no filename provided will display in imagemagick window."
+  puts "width and height required"
+  exit 1
 end
