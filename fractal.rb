a728a258fd6bc5d665c65cfc28bbb207d6472384
Callum Forbes <forbescallum@gmail.com>
2019-05-16 00:35:12 +0100

    add standard escape time coloring

--- fractal.rb
+++ fractal.rb
@@ -6,36 +6,47 @@ class Fractal
     @formula = formula
   end
 
-  def colors1(v, limit)
-    if v > limit # not close enough to M
-      "black"
-    elsif v > 0.005
-      "hsl(#{239-50*Math.sin(((200-v*(200/limit))+50)*v)}, 200, #{80-v*(80/limit)}"
-    else
-      "hsl(#{189-50*Math.sin(500*v)}, 200, #{80-v*(80/limit)}"
-    end
+  def color_at(x, y, max)
+    red, green, blue = color_palette(color_index(iteration_coloring(x, y, max), 2.5, 0), GREY_PALETTE)
+    "##{red.round}#{green.round}#{blue.round}"
   end
 
-  def color_at(x, y, max)
-    z, c = @start_values
-    z = Complex(x, y) if z == :point
-    c = Complex(x, y) if c == :point
-    pow = 1.0
-    (0..max-1).each do |i|
-      r2 = z.real ** 2 + z.imaginary ** 2
-      if r2 > 1000000
-        v = Math.log(r2) / pow
-        return colors1(v, 0.3)
-      end
-      z = @formula.(z, c)
-      pow = pow * 2
+  private
+
+  def color_palette(index, palette)
+    i = index * 100
+    while i > 99
+      i -= 99
     end
-    "black"
+    floor = palette[i.floor]
+    ceiling = palette[i.ceil]
+    diffs = ceiling.zip(floor).map { |(x, y)| (i % 1) * (x - y) }
+    floor.zip(diffs).map(&:sum)
+  end
+
+  grey_palette = 50.downto(1).map { |x| [x * (255 / 50.0)].cycle(3) }
+  GREY_PALETTE = grey_palette + grey_palette.reverse
+
+  def color_index(coloring, k, u0)
+    k * (coloring - u0)
+  end
+
+  def iteration_coloring(x, y, max)
+    z, c = new_start_values(x, y)
+    iterations = 0
+    until z.real ** 2 + z.imaginary ** 2 > 10 || iterations == max
+      z = @formula.(z, c)
+      iterations += 1
+    end
+    iterations / 20.0
+  end
+
+  def new_start_values(x, y)
+    @start_values.map { |v| v == :point ? Complex(x, y) : v }
   end
 end
 
 FRACTALS = {
-  ## name: [start_values, function to iterate]
   mandelbrot: Fractal.new([0, :point], -> (z, c) { z ** 2 + c }),
   burning_ship: 
     Fractal.new([0, :point], -> (z, c) { z.real ** 2 - z.imaginary ** 2 - c }),
