e9f9c6562cb6a77d321eb688c05b5b8e425dd6bf
Callum Forbes <forbescallum@gmail.com>
2019-05-16 19:23:57 +0100

    Restructure to have diff color algos and palettes
    - Fractal now takes coloring_method and palette on init
    - Move high level logic in draw into `run` method

--- draw.rb
+++ draw.rb
@@ -6,16 +6,16 @@ require 'optparse'
 class Plot
   attr_reader :image
 
-  def initialize(color_method)
-    @color_method = color_method
+  def initialize(shader)
+    @shader = shader
   end
 
   def generate(width, height)
     pixels = []
     (0..height-1).each do |y|
       (0..width-1).each do |x|
-        p = Pixel.from_color(@color_method.(x, y))
-        pixels.push(p.red, p.green, p.blue)
+        pixel = Pixel.from_color(@shader.(x,y))
+        pixels.push(pixel.red, pixel.green, pixel.blue)
       end
     end
 
@@ -29,39 +29,51 @@ def scale_point(x, y, width, height, scale)
   return x, y
 end
 
-options = {
+$options = {
   iterations: 50,
   fractal: :mandelbrot,
+  method: :escape_time,
+  palette: :greyscale,
   scale: 4.0,
   position: [-2.4, -2.0]
 }
 
 OptionParser.new do |opts|
   opts.accept(Symbol) { |string| string.to_sym }
-  opts.banner = "Usage: draw.rb width height [options]"
+  opts.banner = "Usage: draw.rb width height [$options]"
 
   opts.on("-i", "--iterations N", Integer,
           "Number of iterations for each pixel. Default: 50") do |n|
-    options[:iterations] = n
+    $options[:iterations] = n
   end
 
   opts.on("-f", "--filename FILENAME",
           "Filename to save image to \
           If none provided will display in window") do |filename|
-    options[:filename] = filename
+    $options[:filename] = filename
   end
 
   opts.on("-t", "--type FRACTAL", Symbol,
           "Type of fractal to draw. Available: " + FRACTALS.keys.join(', ')) do |fractal|
-    options[:fractal] = fractal
+    $options[:fractal] = fractal
+  end
+
+  opts.on("-c", "--coloring-method METHOD", Symbol,
+          "Which coloring algorithm to use. Available: " + COLORING_METHODS.keys.join(', ')) do |method|
+    $options[:method] = method
+  end
+
+  opts.on("-p", "--palette COLORS", Symbol,
+          "Which color palette to use. Available: " + PALETTES.keys.join(', ')) do |palette|
+    $options[:palette] = palette
   end
 
   opts.on("-s", "--scale N", Float, "Zoom level") do |scale|
-    options[:scale] *= scale
+    $options[:scale] *= scale
   end
 
-  opts.on("-x N", Float, "x position") { |x| options[:position][0] += x }
-  opts.on("-y N", Float, "y position") { |y| options[:position][1]  += y }
+  opts.on("-x N", Float, "x position") { |x| $options[:position][0] += x }
+  opts.on("-y N", Float, "y position") { |y| $options[:position][1] += y }
 
   opts.on("-h", "--help", "Prints this help") do
     puts opts
@@ -69,23 +81,31 @@ OptionParser.new do |opts|
   end
 end.parse!
 
-if ARGV[0] && ARGV[1]
-  width, height = ARGV.take(2).map(&:to_i)
-  p = Plot.new(
+def run(width, height, options)
+  fractal = Fractal.new(
+    *FRACTALS[options[:fractal]],
+    COLORING_METHODS[options[:method]],
+    PALETTES[options[:palette]]
+  )
+  plot = Plot.new(
     -> (x, y) do
       x, y = scale_point(x, y, width, height, options[:scale])
       x, y = [x + options[:position].first, y + options[:position].last]
-      FRACTALS[options[:fractal]].color_at(x, y, options[:iterations])
+      fractal.color_at(x, y, options[:iterations])
     end
   )
-  p.generate(width, height)
+  plot.generate(width, height)
+  plot
+end
 
-  if options[:filename]
-    p.image.write(options[:filename])
+if ARGV[0] && ARGV[1]
+  width, height = ARGV.take(2).map(&:to_i)
+  plot = run(width, height, $options)
+  if $options[:filename]
+    plot.image.write($options[:filename])
   else
-    p.image.display
+    plot.image.display
   end
 else
   puts "width and height required"
-  exit 1
 end
--- fractal.rb
+++ fractal.rb
@@ -1,44 +1,31 @@
 require 'cmath'
 
 class Fractal
-  def initialize(start_values, formula)
+  def initialize(start_values, formula, coloring_method, palette)
     @start_values = start_values
     @formula = formula
+    @coloring_method = coloring_method
+    @palette = palette
   end
 
   def color_at(x, y, max)
-    red, green, blue = color_palette(color_index(iteration_coloring(x, y, max), 2.5, 0), GREY_PALETTE)
-    "##{red.round}#{green.round}#{blue.round}"
+    z, c = new_start_values(x, y)
+    r, g, b = color_from_palette(@coloring_method.(z, c, max, @formula))
+                .map { |c| c.to_s.rjust(3, "0") }
+    "##{r}#{g}#{b}"
   end
 
   private
 
-  def color_palette(index, palette)
-    i = index * 100
-    while i > 99
-      i -= 99
+  def color_from_palette(index)
+    len = @palette.length
+     i = index * len
+    while i > len - 1
+      i -= len - 1
     end
-    floor = palette[i.floor]
-    ceiling = palette[i.ceil]
-    diffs = ceiling.zip(floor).map { |(x, y)| (i % 1) * (x - y) }
-    floor.zip(diffs).map(&:sum)
-  end
-
-  grey_palette = 50.downto(1).map { |x| [x * (255 / 50.0)].cycle(3) }
-  GREY_PALETTE = grey_palette + grey_palette.reverse
-
-  def color_index(coloring, k, u0)
-    k * (coloring - u0)
-  end
-
-  def iteration_coloring(x, y, max)
-    z, c = new_start_values(x, y)
-    iterations = 0
-    until z.real ** 2 + z.imaginary ** 2 > 10 || iterations == max
-      z = @formula.(z, c)
-      iterations += 1
-    end
-    iterations / 20.0
+    floor = @palette[i.floor]
+    ceiling = @palette[i.ceil]
+    floor.zip(ceiling).map { |(f, c)| (f + (i % 1) * (c - f)).round }
   end
 
   def new_start_values(x, y)
@@ -46,13 +33,34 @@ class Fractal
   end
 end
 
+module Coloring
+  def self.escape_time(k=2.5, u0=0)
+    -> (z, c, max, formula) do
+      iterations = 0
+      until z.real ** 2 + z.imaginary ** 2 > 10 || iterations == max
+        z = formula.(z, c)
+        iterations += 1
+      end
+      k * (iterations / 20.0 - u0)
+    end
+  end
+end
+
+COLORING_METHODS = {
+  escape_time: Coloring.escape_time
+}
+
+PALETTES = {
+  greyscale: [[0,0,0],[255,255,255],[0,0,0]]
+}
+
 FRACTALS = {
-  mandelbrot: Fractal.new([0, :point], -> (z, c) { z ** 2 + c }),
+  mandelbrot: [[0, :point], -> (z, c) { z ** 2 + c }],
   burning_ship: 
-    Fractal.new([0, :point], -> (z, c) { z.real ** 2 - z.imaginary ** 2 - c }),
-  julia_1: Fractal.new([:point, 0.355+0.355i], -> (z, c) { z ** 2 + c }),
-  julia_2: Fractal.new([:point, 0.37+0.1i], -> (z, c) { z ** 2 + c }),
-  julia_3: Fractal.new([:point, -0.54+0.54i], -> (z, c) { z ** 2 + c }),
-  sin_z: Fractal.new([:point, 2], -> (z, c) { c * CMath.sin(z) }),
-  cos_z: Fractal.new([:point, 2], -> (z, c) { c * CMath.cos(z) })
+    [[0, :point], -> (z, c) { z.real ** 2 - z.imaginary ** 2 - c }],
+  julia_1: [[:point, 0.355+0.355i], -> (z, c) { z ** 2 + c }],
+  julia_2: [[:point, 0.37+0.1i], -> (z, c) { z ** 2 + c }],
+  julia_3: [[:point, -0.54+0.54i], -> (z, c) { z ** 2 + c }],
+  sin_z: [[:point, 2], -> (z, c) { c * CMath.sin(z) }],
+  cos_z: [[:point, 2], -> (z, c) { c * CMath.cos(z) }]
 }
